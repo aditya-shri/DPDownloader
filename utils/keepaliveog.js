e5ea985cffb0b2f4cc9603f6b39a0bd8cbc9a310
Aditya Shrivastava <18643014+aditya-shri@users.noreply.github.com>
2020-10-18 16:17:19 +0530

    Update keepaliveog.js

--- utils/keepaliveog.js
+++ utils/keepaliveog.js
@@ -8,17 +8,17 @@ function keepaliveog() {
 	      var fetcher = axios(`https://gdown.herokuapp.com/api/v1/torrent/list`);
 	      var arr = fetcher.torrents;
               var choice = 0;
-	      console.log(arr);
+	      console.log("Array : ",arr);
 	      for (var i=0; i< arr.length;i++){
 		 if(arr[i].status == "Downloaded"){
 			 choice += 1;
 		 }
 	      }
-	      if(choice == arr.length && arr.length == 0){
+	      if(choice == arr.length || arr.length == 0){
 			console.log("No download remaining");
 			clearInterval(refresh); 
 	      }
-	      console.warn("setInterval applied");
+	      console.log("setInterval applied");
 	      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
 	      console.log("keep alive triggred, status: ", data.status);
 	    }, 1560000);
