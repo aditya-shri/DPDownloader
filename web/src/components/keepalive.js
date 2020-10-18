9dd880a5804cea76814fac4d8948a08f83ac8fec
Aditya Shrivastava <18643014+aditya-shri@users.noreply.github.com>
2020-10-18 12:37:37 +0530

    Update keepalive.js

--- web/src/components/keepalive.js
+++ web/src/components/keepalive.js
@@ -8,22 +8,22 @@ function keepalive() {
 	      var fetcher = axios(`https://gdown.herokuapp.com/api/v1/torrent/list`);
 	      var arr = fetcher["torrents"];
               var choice = 0;
-	      console.warn(arr);
+	      console.log(arr);
 	      for (var i=0; i< arr.length;i++){
 		 if(arr[i].status == "Downloaded"){
 			 choice += 1;
 		 }
 	      }
 	      if(choice == arr.length && arr.length == 0){
-			console.warn("No download remaining");
+			console.log("No download remaining");
 			clearInterval(refresh); 
 	      }
-	      console.warn("setInterval applied");
+	      console.log("setInterval applied");
 	      const data = await axios(`https://ping-pong-sn.herokuapp.com/pingback?link=${site}`);
 	      console.log("keep alive triggred, status: ", data.status);
 	    }, 1560000);
   } else {
-    console.warn("No torrent to download");
+    console.log("No torrent to download");
   }
 }
 
