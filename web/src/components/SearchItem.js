7cb6a24ffc3c64f7e21fd8714c4adb7ba0c18e72
Aditya Shrivastava <18643014+aditya-shri@users.noreply.github.com>
2020-10-23 16:56:31 +0530

    Update SearchItem.js

--- web/src/components/SearchItem.js
+++ web/src/components/SearchItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-//import keepalive from "./keepalive";
+import keepalive from "./keepalive";
 
 function SearchItem({ result, site, api }) {
   const [loading, setLoading] = useState(false);
@@ -23,7 +23,7 @@ function SearchItem({ result, site, api }) {
   const startDownload = () => {
     const sttr = response.torrent.downloadLink;
     fetch(`/api/v1/torrent/download?link=${sttr}`);
-    //keepalive();	
+    keepalive();	
   }
   
   const copyToClipboard = () => {
